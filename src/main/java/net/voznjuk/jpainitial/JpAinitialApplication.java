aac8066d4f48431a85fd307dcae45ef2c9679003
Kostiantyn Vozniuk <Kostiantyn_Vozniuk@epam.com>
2023-07-19 11:55:47 +0300

    Initial commit

---
+++ src/main/java/net/voznjuk/jpainitial/JpAinitialApplication.java
@@ -0,0 +1,13 @@
+package net.voznjuk.jpainitial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAinitialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpAinitialApplication.class, args);
+    }
+
+}
